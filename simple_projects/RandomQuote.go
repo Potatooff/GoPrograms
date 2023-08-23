bfdb48202e53dbba6de337f25b01087f698ed713
Cloud <112786274+Potatooff@users.noreply.github.com>
2023-08-23 11:16:19 -0400

    Update RandomQuote.go
    added semi colons

--- simple_projects/RandomQuote.go
+++ simple_projects/RandomQuote.go
@@ -1,12 +1,12 @@
 package main;
 import (
-  "fmt"
-  "math/rand"
+  "fmt";
+  "math/rand";
 )
 
 func main() {
 	// Run answer is also main function just lazy to clean it!
-	answer()
+	answer();
 }
 
 // Choose a random quote from "quotes"
@@ -19,18 +19,18 @@ func randomize()string {
 		"I love chickens",
 		"I eat chickens",
 	}
-	RandomQuote := rand.Intn(len(quotes))
-	fmt.Printf("\nRandom Quote of the day: %v", quotes[RandomQuote])
-	fmt.Print("\nGenerate another one?\n1- yes\n2- No\nUser:");fmt.Scan(&answer)
-	return answer
+	RandomQuote := rand.Intn(len(quotes));
+	fmt.Printf("\nRandom Quote of the day: %v", quotes[RandomQuote]);
+	fmt.Print("\nGenerate another one?\n1- yes\n2- No\nUser:");fmt.Scan(&answer);
+	return answer;
 }
 
 // Check if answer is 1 or 2 before running randomizer again!
 func answer() {
 	var answer string;
-	fmt.Println("Welcome to A Random Quote randomizer.")
+	fmt.Println("Welcome to A Random Quote randomizer.");
 	answer = randomize()
 	for answer == "1" {
-		answer = randomize()
+		answer = randomize();
 	}
 }
