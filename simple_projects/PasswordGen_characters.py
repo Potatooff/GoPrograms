dc6d0ad1efe0a2f23d71183513ec4b560ec13d41
Cloud <112786274+Potatooff@users.noreply.github.com>
2023-08-24 00:21:05 -0400

    Create PasswordGen_characters.py
    Where did i got those characters!

---
+++ simple_projects/PasswordGen_characters.py
@@ -0,0 +1,7 @@
+import string # Import string because it already contains premade characters variables
+
+# This give us the characters needed for our secure password generator!
+# Be careful of the quotes and backslashes!
+print(string.ascii_letters)
+print(string.digits)
+print(string.punctuation)
